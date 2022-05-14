8f78366a831e643c605039e2426e9097ecaf7a0f
MA0HS <oralia.hernandez@hotmail.com>
2022-05-13 22:33:06 -0500

    test de despliegue inicial

---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    
+    <p>Test de Despliegue</p>
+    
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
